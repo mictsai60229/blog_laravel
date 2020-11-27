d2350676f8903a8ecdf552898073d4d337583b3e
mictsai60229 <mictsai@nlplab.cc>
2020-11-27 17:28:18 +0800

    feat: BlogResponses Validation

--- app/Http/Controllers/BlogResponseController.php
+++ app/Http/Controllers/BlogResponseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\BlogResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 
 class BlogResponseController extends Controller
@@ -16,34 +17,38 @@ class BlogResponseController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->except(['index']);
+        $this->middleware('auth');
     }
 
     public function create(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'blog_id' => 'required|exists:blogs,id',
+            'content' => 'required',
+        ]);
+
+        if ($validator->fails()){
+            return response()->json(['status'=> "fail"]);
+        }
+
         $comment = $request->input('content');
         $blog_id = $request->input('blog_id');
         $user = $request->user();
 
-        $response_data = [];
+        $response_data = array();
         
-        if ($comment !== NULL && $blog_id !== NULL){
-            $blog_response = new BlogResponse;
-            $blog_response->user_id = $user->id;
-            $blog_response->name = $user->name;
-            $blog_response->blog_id = $blog_id;
-            $blog_response->content = $comment;
+        $blog_response = new BlogResponse;
+        $blog_response->user_id = $user->id;
+        $blog_response->name = $user->name;
+        $blog_response->blog_id = $blog_id;
+        $blog_response->content = $comment;
 
-            $blog_response->save();
+        $blog_response->save();
 
-            $response_data['id'] = $blog_response->id;
-            $response_data['status'] = 'success';
-            $response_data['name'] = $user->name;
-            $response_data['created_at'] = Carbon::now()->toDateTimeString();
-        }
-        else{
-            $response_data['status'] = 'fail';
-        }
+        $response_data['id'] = $blog_response->id;
+        $response_data['status'] = 'success';
+        $response_data['name'] = $user->name;
+        $response_data['created_at'] = Carbon::now()->toDateTimeString();
 
         return response()->json($response_data);
     }
